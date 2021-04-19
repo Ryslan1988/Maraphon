976ed320fe6a967f5225a87dea975eb5421bc1d7
Ryslan Pynzar <biopyn@yandex.ru>
2021-04-19 10:38:17 +0300

    changes day 8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -1,7 +1,28 @@
 package day8;
 
+import java.util.Arrays;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        String get = "";
+
+        long timeBefour = System.currentTimeMillis();
+
+        for (int i = 0; i < 20000; i++)
+            get = get + i + " ";
+        long timeAfter = System.currentTimeMillis();
+
+        System.out.println("Время с классом String: " + (timeAfter - timeBefour));
+        // System.out.println(get); скрыл что бы были наглядно видны показатели
+
+        StringBuilder a = new StringBuilder("");
+        timeBefour = System.currentTimeMillis();
+
+        for (int i = 0; i < 20000; i++)
+            a.append(i).append(" ");
+        timeAfter = System.currentTimeMillis();
+        System.out.println("Время с классом StringBuilder: " + (timeAfter - timeBefour));
+        //System.out.println(a); скрыл что бы наглядно были видны показатели
     }
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,7 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane("Aerbus", 2002, 44, 33300);
+        System.out.println(airplane);
     }
 }
