2b97c8aab1180c97e4ec7745c3626b84fc38c491
Ryslan Pynzar <biopyn@yandex.ru>
2021-04-29 10:43:22 +0300

    changes day 14 tasks

--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,40 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.lang.reflect.Field;
+import java.util.*;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        System.out.println(parseFileToStringList());
     }
+
+    public static List<String> parseFileToStringList() {
+
+        File file = new File("People");
+        List<String> peope = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] person = line.split(" ");
+
+                if (Integer.parseInt(person[1]) < 0)
+                    throw new IllegalArgumentException();
+                peope.add(line);
+            }
+            return peope;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found ");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Not correctly");
+        }
+        return null;
+    }
+
+
 }
+
--- src/main/java/day14/Task3.java
+++ src/main/java/day14/Task3.java
@@ -1,7 +1,43 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        System.out.println(parseFileToObjList());
     }
+
+    public static List<Person> parseFileToObjList() {
+
+        File file = new File("People");
+        List<Person> peope = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(file);
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] person = line.split(" ");
+
+                if (Integer.parseInt(person[1]) < 0)
+                    throw new IllegalArgumentException();
+                Person current = new Person(person[0], Integer.parseInt(person[1]) );
+                peope.add(current);
+            }
+            return peope;
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found ");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Not correctly");
+        }
+        return null;
+    }
+
+
 }
+
+
